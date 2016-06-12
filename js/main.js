3d0e86af5d53df7b2667b0c6a764121d28dcad77
akeem-s <akeemstreet@gmail.com>
2016-06-12 14:34:52 -0400

    Add js file and unmute button that fades away after ten seconds

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("video").prop('muted', true);
+
+  $("#unmute-button").click( function (){
+    if( $("video").prop('muted') ) {
+      $("video").prop('muted', false);
+    } else {
+      $("video").prop('muted', true);
+    }
+  });
+
+  $('#unmute-button').delay(5000).fadeOut('slow');
+});
