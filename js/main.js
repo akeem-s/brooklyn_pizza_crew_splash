f81260772225337b148682ef2ca8b0437085ac4d
Brian Bier <briansbier@gmail.com>
2016-06-14 13:36:33 -0400

    Style the main header page

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-$(document).ready(function() {
-  $('#unmute-button').hide()
-  $("#mailchimp").hide();
-  $("video").prop('muted', true);
-debugger;
-  $("#unmute-button").click( function (){
-    if( $("video").prop('muted') ) {
-      $("video").prop('muted', false);
-    } else {
-      $("video").prop('muted', true);
-    }
-  });
-
-  $("#volume-on").click(function(event){
-      $("video").prop('muted', false);
-      $("#volume-on").hide()
-      $("#volume-off").show()
-  })
-
-  $("#volume-off").click(function(event){
-      $("video").prop('muted', true);
-      $("#volume-off").hide()
-      $("#volume-on").show()
-  })
+// $(document).ready(function() {
+//   $('#unmute-button').hide()
+//   $("#mailchimp").hide();
+//   $("video").prop('muted', true);
   
-  $('video').click(function(){this.paused?this.play():this.pause();});
+//   $("#unmute-button").click( function (){
+//     if( $("video").prop('muted') ) {
+//       $("video").prop('muted', false);
+//     } else {
+//       $("video").prop('muted', true);
+//     }
+//   });
 
-  $("#video-section").mousemove(function(event){
-    $('#unmute-button').show();
-    $("#mailchimp").show();
-    $("#mailchimp").delay(2000).fadeOut('slow');
-    $('#unmute-button').delay(2000).fadeOut('slow');
-  })
-});
+//   $("#volume-on").click(function(event){
+//       $("video").prop('muted', false);
+//       $("#volume-on").hide()
+//       $("#volume-off").show()
+//   })
+
+//   $("#volume-off").click(function(event){
+//       $("video").prop('muted', true);
+//       $("#volume-off").hide()
+//       $("#volume-on").show()
+//   })
+  
+//   $('video').click(function(){this.paused?this.play():this.pause();});
+
+//   $("#video-section").mousemove(function(event){
+//     $('#unmute-button').show();
+//     $("#mailchimp").show();
+//     $("#mailchimp").delay(2000).fadeOut('slow');
+//     $('#unmute-button').delay(2000).fadeOut('slow');
+//   })
+// });
